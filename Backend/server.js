341616990e06c289c1c383cc9c14d977f12f3797
SameekshaVarshney08 <sameekshavarshney894@gmail.com>
2024-06-12 16:32:15 +0530

    abc

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,8 @@ const app=require('./app.js')
 
 const url='mongodb+srv://SAMEEKSHA:9qcRTvmp06yX1NxL@cluster0.zti6gp3.mongodb.net/testDB'
 
-let dbLink=url.replace("$_USERNAME_$",databaseUser);
-dbLink=dbLink.replace("$_PASSWORD_$",databasePassword);
-dbLink=dbLink.replace("$_DB_NAME_$",databaseName);
 
-mongoose.connect(dbLink)
+mongoose.connect(url)
 .then(() => console.log('Database Connected!'));
 
 app.listen(8000,()=> {

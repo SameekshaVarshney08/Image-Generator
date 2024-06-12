ae604e88b5e2a45682359f14a7700b2eff405556
SameekshaVarshney08 <sameekshavarshney894@gmail.com>
2024-06-12 16:19:09 +0530

    Changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,35 +1,38 @@
 const express = require("express");
-const imageRouter = require('./routes/imageRouter.js');
+const imageRouter = require("./routes/imageRouter.js");
 const authRouter = require("./routes/authRouter.js");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
 const app = express();
 
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 app.use(express.json());
-app.get("/",(req,res)={res.send({message:"This is Image-Generator-Backend"})})
+app.get("/", (req, res) => {
+  res.send({ message: "This is Image-Generator-Backend" });
+});
 
-app.use('/api/v1/auth', authRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use((req, res, next) => {
-    let token;
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
-        token = req.headers.authorization.split(' ')[1];
-    }
-    try{
-        jwt.verify(token, 'my-secret-123');
-        next();
-    }
-    catch(err){
-        return res.status(401).json({
-            status: 'fail',
-            message: 'Login required',
-        })
-    }
-})
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  try {
+    jwt.verify(token, "my-secret-123");
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      status: "fail",
+      message: "Login required",
+    });
+  }
+});
 
-app.use('/api/v1/images', imageRouter);
+app.use("/api/v1/images", imageRouter);
 
 module.exports = app;
- 
